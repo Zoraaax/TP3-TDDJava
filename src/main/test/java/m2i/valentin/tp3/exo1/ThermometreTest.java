9570c6fd7d017d0ed79f831ac99f2662b040dae1
Valentin Thibault <valentin.thlt@gmail.com>
2024-01-05 15:21:41 +0100

    refactor: instanticiation of thermometre class

--- src/main/test/java/m2i/valentin/tp3/exo1/ThermometreTest.java
+++ src/main/test/java/m2i/valentin/tp3/exo1/ThermometreTest.java
@@ -1,14 +1,21 @@
 package m2i.valentin.tp3.exo1;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ThermometreTest {
 
+    private Thermometre thermometre;
+
+    @BeforeEach
+    public void setUp() {
+        thermometre = new Thermometre();
+    }
+
     @Test
     public void empty_array_should_return_0() {
-        Thermometre thermometre = new Thermometre();
 
         int result = thermometre.checkTemperature(new int[]{});
 
@@ -17,7 +24,6 @@ public class ThermometreTest {
 
     @Test
     public void array_with_one_element_should_return_this_element() {
-        Thermometre thermometre = new Thermometre();
 
         int result = thermometre.checkTemperature(new int[]{4});
 
