fd69d45e71dfaccdfc8e476ec6a255b22eae7c0c
Valentin Thibault <valentin.thlt@gmail.com>
2024-01-05 16:35:27 +0100

    feat: Thermometre :sparkles:

--- src/main/java/m2i/valentin/tp3/exo1/Thermometre.java
+++ src/main/java/m2i/valentin/tp3/exo1/Thermometre.java
@@ -2,7 +2,11 @@ package m2i.valentin.tp3.exo1;
 
 public class Thermometre {
 
-    public int checkTemperature(int[] temperature) {
+    public int checkTemperature(int[] temperature) throws IllegalArgumentException {
+
+        if (temperature.length > 1000) {
+            throw new IllegalArgumentException("Le tableau des températures doit être inférieur à 1000 éléments.");
+        }
 
         if (temperature.length == 0) {
             return 0;
